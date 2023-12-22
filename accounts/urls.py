aa2e84b671b4869e1d943fc740d2a7a0dc112808
akiyamakana <odazai3633@gmail.com>
2023-12-23 04:37:55 +0900

    コメント消去

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,8 +10,6 @@ urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
     path("login/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("<str:username>/", views.UserProfileView.as_view(), name="user_profile"),
     # path('<str:username>/follow/', views.FollowView.as_view(), name='follow'),
     # path('<str:username>/unfollow/', views.UnFollowView, name='unfollow'),
